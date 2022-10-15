d44ab53a2374b1e905cfca073211e8f2f1f3c4b6
imjjun0117 <imjjun0117@naver.com>
2022-10-15 11:59:59 +0900

    회원관리 기능 예제

--- src/main/java/com/example/hellospring/controller/HelloController.java
+++ src/main/java/com/example/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,4 +23,37 @@ public class HelloController {
         return "hello-template";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        //ReponseBody
+        //HTTP의 BODY에 문자 내용을 직접 반환
+        //viewResolver 대신에 HttpMessageConverter가 동작
+        //기본 문자 처리 : StringHttpMessageConverter
+        //기본 객체 처리 : MappingJackson2HttpMessageConverter (JSON) 반환
+        //byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
+        //반환 데이터는 HTTP Accept 헤더에서 설정할 수 있음
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+
+            return this.name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
 }
---
+++ src/main/java/com/example/hellospring/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package com.example.hellospring.repository;
+
+import com.example.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    // 데이터 저장소가 선정되지 않아 우선, 리포지토리는 인터페이스로 구현 클래스를 변경할 수 있도록 설계
+    // 데이터 저장소는 RDB, NoSQL 등등 다양한 저장소를 고민중인 상황으로 가정
+    // 개발을 진행하기 위해서 초기 개발 단계에서는 구현체로 가벼운 메모리 기반의 데이터 저장소 사용
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
